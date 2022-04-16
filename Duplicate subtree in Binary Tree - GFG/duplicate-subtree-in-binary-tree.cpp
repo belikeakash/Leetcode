21dac16e473a450219881d5226477ca1d2789f54
Akash <76730100+belikeakash@users.noreply.github.com>
2022-04-16 22:42:33 +0530

    Added solution - LeetHub

---
+++ Duplicate subtree in Binary Tree - GFG/duplicate-subtree-in-binary-tree.cpp
@@ -0,0 +1,146 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(char x) {
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node *root = new Node(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current Node
+			currNode->left = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+ // } Driver Code Ends
+/*The structure of the Binary Tree Node  is
+struct Node
+{
+  char data;
+  struct Node* left;
+  struct Node* right;
+};*/
+
+class Solution {
+  public:
+    /*This function returns true if the tree contains 
+    a duplicate subtree of size 2 or more else returns false*/
+    string func(Node *root, unordered_map<string,int>&m, unordered_map<string,int>&g) {
+        if(root==NULL) return "";
+        int x = 0;
+        string s = to_string(root->data);
+        x+=1;
+        s+="*";
+        string j = func(root->left,m,g);
+        s+=j;
+        x+=j.size();
+        s+="*";
+        string k = func(root->right,m,g);
+        s+=k;
+        x+=k.size();
+        
+        m[s]++;
+        g[s] = x;
+        return s;
+    }
+    int dupSub(Node *root) {
+         unordered_map<string,int>m;
+         unordered_map<string,int>g;
+         func(root,m,g);
+         int ans = 0;
+         for(auto x:m) {
+             if(x.second>1 && g[x.first]>=2) {
+                //  cout<<x.first<<" "<<g[x.first]<<endl;
+                 ans = 1;
+                 break;
+             }
+         }
+         return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin >> t;
+	//cout << t << "\n";
+	while (t--)
+	{
+		string treeString;
+		getline(cin >> ws, treeString);
+		struct Node* root = buildTree(treeString);
+		Solution ob;
+		cout << ob.dupSub(root) << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
