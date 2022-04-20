c9435a26a428bf9bf55ad3e85cf905de302fb755
Akash <76730100+belikeakash@users.noreply.github.com>
2022-04-20 10:17:21 +0530

    Time: 63 ms (6.43%), Space: 23.2 MB (92.61%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,38 +10,28 @@
 
 class Solution {
 public:
-    void Traverse(TreeNode* root, TreeNode* x,vector<TreeNode*>&v) {
-        if(root==NULL) return;
-        TreeNode* nroot = root;
+    TreeNode* ans;
+    void func(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* dummy = root;
         while(root!=NULL) {
-            v.push_back(nroot);
-                if(nroot->val==x->val){
-                break; }
-            else if(nroot->val > x->val) {
-                nroot=nroot->left;
+            if(p->val <= root-> val && q->val >= root->val) {
+                ans = root;
+                break;
+            }
+            else if(p->val > root->val) {
+                root=root->right;
             }
             else {
-                nroot=nroot->right;
+                root=root->left;
             }
         }
-        
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>v1,v2;
-        Traverse(root,p,v1);
-        Traverse(root,q,v2);
-        TreeNode* ans;
-        for(auto x:v1) {
-            cout<<x->val<<" ";
+        
+        if(p->val > q->val) {
+            func(root,q,p);
         }
-        cout<<endl;
-        for(auto x:v2) {
-            cout<<x->val<<" ";
-        }
-        for(int i=0;i<min(v1.size(),v2.size());i++) {
-            if(v1[i]->val==v2[i]->val) ans = v1[i];
-            else break;
-        }
+        else func(root,p,q);
         return ans;
     }
 };
