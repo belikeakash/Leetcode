a1ba2a45a1d8edf7ea4c5beb3225378e957fc554
Akash <76730100+belikeakash@users.noreply.github.com>
2022-04-30 16:13:53 +0530

    Time: 6 ms (41.91%), Space: 7.8 MB (61.04%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> relativeSortArray(vector<int>& a2, vector<int>& a1) {
-        vector<int>ans;
-        for(int i=0;i<a1.size();i++) {
-            for(int j=0;j<a2.size();j++) {
-                if(a1[i]==a2[j]) {ans.push_back(a2[j]); a2[j] = -1;}
+    vector<int> relativeSortArray(vector<int>& a1, vector<int>& a2) {
+        map<int,int>m;
+        for(auto x:a1) m[x]++;
+        int i = 0;
+        for(auto x:a2) {
+            while(m[x]>0) {
+                a1[i]=x;
+                i++;
+                m[x]--;
             }
         }
-        vector<int>ans2;
-        
-        for(int i=0;i<a2.size();i++) if(a2[i]!=-1) ans2.push_back(a2[i]);
-        sort(ans2.begin(),ans2.end());
-        for(auto x:ans2) {
-            ans.push_back(x);
+        for(auto x:m) {
+            while(x.second-- >0) a1[i++]=x.first;
         }
-        return ans;
+        return a1;
     }
 };
