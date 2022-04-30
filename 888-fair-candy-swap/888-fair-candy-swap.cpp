644b7f27b5b209f7b22c92afa79125f6df4982bb
Akash <76730100+belikeakash@users.noreply.github.com>
2022-04-30 22:00:57 +0530

    Time: 260 ms (14.34%), Space: 52 MB (18.19%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.cpp
+++ 888-fair-candy-swap/888-fair-candy-swap.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& a1, vector<int>& a2) {
+    vector<int> fairCandySwap(vector<int>& a, vector<int>& b) {
         int s1=0,s2=0;
-        for(auto x:a1) s1+=x;
-        for(auto x:a2) s2+=x;
-        sort(a1.begin(),a1.end());
-        sort(a2.begin(),a2.end());
+        unordered_map<int,int>m;
+        for(auto x:a) s1+=x;
+        for(auto x:b) {s2+=x; m[2*x]++;}
         int d = s1-s2;
         vector<int>ans;
-        bool flag = 0;
-        for(int i=0;i<a1.size();i++) {
-            int l = 0,r=a2.size()-1;
-            while(l<=r) {
-                int mid = l+(r-l)/2;
-                cout<<a1[i]-a2[mid]<<" ";
-                if(2*(a1[i]-a2[mid])==d) {
-                    flag = 1;
-                    cout<<a1[i]-a2[mid]<<endl;
-                    ans.push_back(a1[i]);
-                    ans.push_back(a2[mid]);
-                    break;
-                }
-                else if(2*(a1[i]-a2[mid])<d) {
-                    r = mid-1;
-                }
-                else {
-                    l = mid+1;
-                }
-            }
-            if(flag==1) {
+        for(auto x:a) {
+            if(m[2*x - d]>0) {
+                ans.push_back(x);
+                ans.push_back((2*x - d)/2);
                 break;
             }
         }
-        cout<<endl;
         return ans;
     }
 };
