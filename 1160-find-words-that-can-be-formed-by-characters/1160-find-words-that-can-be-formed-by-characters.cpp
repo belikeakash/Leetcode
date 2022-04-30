2fa524915ca87f7df7de1fa052c61f5251bfebf3
Akash <76730100+belikeakash@users.noreply.github.com>
2022-04-30 14:31:41 +0530

    Time: 91 ms (53.74%), Space: 22.8 MB (51.43%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int countCharacters(vector<string>& v, string s) {
-        map<char,int>m;
+        vector<int>m(27,0);
         int ans = 0;
-        for(int i=0;i<s.size();i++) m[s[i]]++;
+        for(int i=0;i<s.size();i++) m[s[i]-'a']++;
         for(int i=0;i<v.size();i++) {
             string x = v[i];
             bool flag = 1;
-            map<char,int>p;
+            vector<int>p(27,0);
             for(int j=0;j<x.size();j++) {
-                p[x[j]]++;
-                if(p[x[j]] > m[x[j]]) {flag = 0; continue;}
-                if(m[x[j]]>0) {
+                p[x[j]-'a']++;
+                if(p[x[j]-'a'] > m[x[j]-'a']) {flag = 0; continue;}
+                if(m[x[j]-'a']>0) {
                     continue;
                 }
                 else {
