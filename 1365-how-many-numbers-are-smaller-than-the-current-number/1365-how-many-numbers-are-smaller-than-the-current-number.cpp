88f1ddcd307ddb90ea22e1d6bb874b3dac10ec34
Akash <76730100+belikeakash@users.noreply.github.com>
2022-04-30 13:35:10 +0530

    Time: 13 ms (67.42%), Space: 10.5 MB (19.87%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -7,12 +7,12 @@ public:
             b[i] = a[i];
         }
         sort(a.begin(),a.end());
-        vector<int>ans(n,0);
+        // vector<int>ans(n,0);
         map<int,int>m;
         for(int i=n-1;i>=0;i--) m[a[i]] = i;
         for(int i=0;i<n;i++) {
-            ans[i] = m[b[i]];
+            a[i] = m[b[i]];
         }
-        return ans;
+        return a;
     }
 };
