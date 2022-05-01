2d7fa30c3dae0d2fc7e62a1169ff08a0df254466
Akash <76730100+belikeakash@users.noreply.github.com>
2022-05-01 13:28:33 +0530

    Time: 59 ms (57.22%), Space: 9.4 MB (42.17%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     vector<int> minOperations(string s) {
-        vector<int>ans;
-        for(int i=0;i<s.size();i++) {
-            int cnt = 0;
-            for(int j=0;j<s.size();j++) {
-                if(s[j]=='1' && j!=i) cnt+=abs(j-i);
+        int n = s.size();
+        vector<int>ans(n,0);
+        vector<int>l(n,0),r(n,0);
+        int x = 0;
+        for(int i=0;i<n;i++) {
+            if(s[i]=='1') {
+                x++;
+                ans[0]+=i;
             }
-            ans.push_back(cnt);
+            l[i]=x;
+        }
+        x=0;
+        for(int i=n-1;i>=0;i--) {
+            if(s[i]=='1') x++;
+            r[i]=x;
+        }
+        // reverse(r.begin(),r.end());
+        x=0;
+        // for(int i=1;i<n;i++) {
+        //     if(s[i]=='1') ans[0]+=i;
+        // }
+        // ans[0] = x;
+        for(auto x:l) cout<<x<<" ";
+        cout<<endl;
+        for(auto x:r) cout<<x<<" ";
+        for(int i=1;i<n;i++) {
+            ans[i] = ans[i-1] + l[i-1] - r[i];
+            // if(s[i]=='1') ans[i]-=2;
         }
         return ans;
     }
