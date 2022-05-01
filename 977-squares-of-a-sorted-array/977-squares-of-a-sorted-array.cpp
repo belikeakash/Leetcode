d7367e3bf6cfc01249c4fd3e9f1c492019599b6e
Akash <76730100+belikeakash@users.noreply.github.com>
2022-05-01 19:55:58 +0530

    Time: 34 ms (80.28%), Space: 33.8 MB (5.37%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& a) {
-        multiset<int>v;
+        vector<int>v(10004,0);
         int n = a.size();
-        for(auto x:a) v.insert(abs(x));
+        for(auto x:a) v[abs(x)]++;
         vector<int>ans;
-        for(auto x:v) {
-            ans.push_back(x*x);
+        for(int i=0;i<10004;i++) {
+            if(v[i]>0) {
+                while(v[i]-->0) ans.push_back(i*i);
+            }
         }
         return ans;
     }
