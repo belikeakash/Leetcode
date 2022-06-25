e4533e5020cd0b4a1d0ea95589e4f01a75127b77
Akash <76730100+belikeakash@users.noreply.github.com>
2022-06-25 18:20:35 +0530

    Time: 881 ms (5.06%), Space: 59.1 MB (37.45%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int maxArea(vector<int>& a) {
         int n = a.size();
-        int ans = 0;
+        int ans = min(a[0],a[n-1])*(n-1);
         int i = 0,j = n-1;
-        while(i<j) {
-            int h = min(a[i],a[j]);
-            // cout<<ans<<" ";
-            ans = max(ans,(j-i)*h);
-            while(a[i]<=h && i<j) i++;
-            while(a[j]<=h && i<j) j--;
-            // cout<<ans<<"  "<<h<<endl;
-            cout<<ans<<" "<<i<<" "<<j<<endl;
+        while(i!=j) {
+            ans = max(ans,min(a[i],a[j])*(j-i));
+            if(a[i]>a[j]) {
+                j--;
+            }
+            else {
+                i++;
+            }
+            cout<<ans<<" ";
         }
         return ans;
     }
