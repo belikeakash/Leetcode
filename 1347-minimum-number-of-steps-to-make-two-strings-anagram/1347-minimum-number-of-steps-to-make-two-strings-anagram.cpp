b314acca971311163f360178efd7800836a49311
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-08 10:48:46 +0530

    Time: 53 ms (91.53%), Space: 16.6 MB (75.49%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        vector<int>v1(27,0);
-        vector<int>v2(27,0);
+        vector<int>a(26,0),b(26,0);
         for(auto x:s) {
-            v1[x-'a']++;
+            a[x-'a']++;
         }
         for(auto x:t) {
-            v2[x-'a']++;
+            b[x-'a']++;
         }
         int ans = 0;
         for(int i=0;i<26;i++) {
-            ans += max(0,v1[i]-v2[i]);
+            if(b[i]>a[i]) ans += b[i]-a[i];
         }
         return ans;
     }
