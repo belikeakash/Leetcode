6ff5d815f38045a4b888f76c8e9983bbbbb65268
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-08 17:15:18 +0530

    Time: 19 ms (7.48%), Space: 8.4 MB (65.54%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& v, string p) {
-        int i = 0;
-        unordered_map<char,int>m;
-        int n = p.size();
-        string os = "";
-        for(auto x:p) {
-            if(m.count(x)) os+=to_string(m[x]);
-            else {
-                os+=to_string(i);
-                m[x]=i;
-                i++;
-            }
+    vector<string> findAndReplacePattern(vector<string>& v, string s) {
+        string a1 = "";
+        map<char,int>m1;
+        int p = 1;
+        for(int i=0;i<s.size();i++) {
+            if(m1[s[i]]==0) {m1[s[i]] = p; p++;}
+            a1+=to_string(m1[s[i]]);
+            a1+="*";
+            // cout<<s[i]-s[i-1]<<" ";
         }
-        cout<<os<<endl;
-        vector<string>ans;
-        for(auto s:v) {
-                if(s.size()!=n) continue;
-                int i = 0;
-                unordered_map<char,int>m;
-            // int n = p.size();
-                string op = "";
-                for(auto x:s) {
-                if(m.count(x)) op+=to_string(m[x]);
-                else {
-                    op+=to_string(i);
-                    m[x]=i;
-                    i++;
-                }
-            }
-            if(op==os) ans.push_back(s);
-        }
+        cout<<a1<<"_";
         
+        vector<string>ans;
+        for(auto x:v) {
+            string a2 = "";
+            p = 1;
+            map<char,int>m2;
+            for(int i=0;i<x.size();i++) {
+               if(m2[x[i]]==0) {m2[x[i]] = p; p++;}
+                a2+=to_string(m2[x[i]]);
+                a2+="*";
+            }   
+            cout<<a2<<" ";
+            if(a2==a1) ans.push_back(x);
+        }
         return ans;
     }
 };
