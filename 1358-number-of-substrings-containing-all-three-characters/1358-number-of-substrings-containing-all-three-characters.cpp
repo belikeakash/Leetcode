ae785f97c3ac170a8c83def79eac5dad30e87207
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-09 14:54:35 +0530

    Time: 52 ms (50.25%), Space: 8.5 MB (86.76%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int c1=0,c2=0,c3=0;
+        vector<int>a(3,0);
         for(int i=0;i<=2;i++) {
-            if(s[i]=='a') c1++;
-            else if(s[i]=='b') c2++;
-            else if(s[i]=='c') c3++;
+            a[s[i]-'a']++;
         }
-        int i = 0,j = 2;
-        // if(c1>=0 && c2>=0 && c3>=0) ans+=n-2;
-        
-        int ans = 0;
+        int cnt = 0;
         int n = s.size();
-        while(j<n) {
-            if(c1>=1 && c2>=1 && c3>=1){
+        int ans = 0;
+        int i = 0,j=2;
+        while(i<j && j<n) {
+            cnt = 0;
+            for(int i=0;i<=2;i++) {
+            if(a[i]>0) cnt++;
+        }
+            if(cnt==3) {
                 ans+=n-j;
-                cout<<j<<" ";
-                if(s[i]=='a') c1--;
-                if(s[i]=='b') c2--;
-                if(s[i]=='c') c3--;
-                i++;
                 
+                a[s[i]-'a']--;
+                i++;
             }
-            else {
-                j++;
-                if(s[j]=='a') c1++;
-                if(s[j]=='b') c2++;
-                if(s[j]=='c') c3++;
-            }
-            
+            else {j++; if(j<n) a[s[j]-'a']++;}
         }
         return ans;
+        
     }
 };
