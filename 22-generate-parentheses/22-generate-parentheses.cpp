3c2eb89c14aafe1c987d8fe03abd4602ba14cdca
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-12 15:21:31 +0530

    Time: 113 ms (5.02%), Space: 40 MB (5.04%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -5,32 +5,31 @@ public:
             v.push_back(s);
             return;
         }
-        if(i>0) {func(i-1,j+1,v,s+'(');}
-        if(j>0) {func(i,j-1,v,s+')');}
-        
+        if(j>0) func(i,j-1,v,s+')');
+        if(i>0) func(i-1,j,v,s+'(');
         
     }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
         vector<string>v;
-        func(n,0,v,"");
+        func(n,n,v,"");
         for(auto x:v) {
             cout<<x<<" ";
         }
         
-        // for(auto x:v) {
-        //     string s = x;
-        //     stack<char>st;
-        //     for(int i=0;i<s.size();i++) {
-        //         if(st.size()==0) st.push(s[i]);
-        //         if(st.top()=='(' && s[i]==')') st.pop();
-        //         else {
-        //             st.push(s[i]);
-        //         }
-        //     }
-        //     cout<<st.size()<<" ";
-        //     if(st.size()==0) ans.push_back(x);
-        // }
-        return v;
+        for(auto x:v) {
+            string s = x;
+            stack<char>st;
+            for(int i=0;i<s.size();i++) {
+                if(st.empty()) st.push(s[i]);
+                if(st.top()=='(' && s[i]==')') st.pop();
+                else {
+                    st.push(s[i]);
+                }
+            }
+            cout<<st.size()<<" ";
+            if(st.size()==1) ans.push_back(x);
+        }
+        return ans;
     }
 };
