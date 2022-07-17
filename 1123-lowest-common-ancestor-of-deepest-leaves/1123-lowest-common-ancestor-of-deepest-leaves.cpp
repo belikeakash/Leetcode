19d547f41e0a69449b3c8080f9628fcfd68ad42d
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-17 21:41:01 +0530

    Time: 11 ms (95.19%), Space: 21.2 MB (34.41%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -11,30 +11,22 @@
  */
 class Solution {
 public:
-    TreeNode* findLCA(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL || root==p || root==q) return root;
-        TreeNode* left = findLCA(root->left,p,q);
-        TreeNode* right = findLCA(root->right,p,q);
-        if(left==NULL) return right;
-        else if(right==NULL) return left;
-        else return root;
+    int func(TreeNode* root) {
+        if(root==NULL) return 0;
+        return max((1+func(root->left)),(1+func(root->right)));
     }
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        vector<TreeNode*>v;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()) {
-            int n = q.size();
-            v.clear();
-            for(int i=0;i<n;i++) {
-                auto x = q.front();
-                v.push_back(x);
-                q.pop();
-                if(x->left) q.push(x->left);
-                if(x->right) q.push(x->right);
-            }
+        if(root==NULL) return root;
+        int l = func(root->left);
+        int r = func(root->right);
+        if(l==r) {
+            return root;
         }
-        
-        return findLCA(root,v[0],v[v.size()-1]);
+        else if(l<r) {
+            return lcaDeepestLeaves(root->right);
+        }
+        else {
+            return lcaDeepestLeaves(root->left);
+        }
     }
 };
