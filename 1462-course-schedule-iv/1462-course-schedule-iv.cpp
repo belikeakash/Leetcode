c2e3cbf23a534459cb8f3d75a4b747ea78802ca0
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-21 21:45:46 +0530

    Time: 463 ms (47.10%), Space: 60.5 MB (84.67%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool DFS(vector<vector<int>>&ar, int node, int temp, vector<int>&vis) {
+        if(node==temp) return 1;
+        vis[node] = 1;
+        for(auto x:ar[node]) {
+            if(vis[x]==0) {
+                if(DFS(ar,x,temp,vis)) return 1;
+            }
+        }
+        return 0;
+    }
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& v, vector<vector<int>>& q) {
+        vector<int>vis(n,0);
+        vector<vector<int>>ar(n);
+        for(int i=0;i<v.size();i++) {
+            int x = v[i][0];
+            int y = v[i][1];
+            ar[x].push_back(y);
+        }
+        vector<bool>ans;
+        for(int i=0;i<q.size();i++) {
+            for(int i=0;i<n;i++) vis[i] = 0;
+            int x = q[i][0];
+            int y = q[i][1];
+            if(DFS(ar,x,y,vis)) ans.push_back(1);
+            else ans.push_back(0);
+        }
+        return ans;
+    }
+};
