c903e3f1511ebeac124afd6cd55377f13bcb27f6
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-22 22:31:39 +0530

    Time: 18 ms (73.54%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,35 +3,39 @@ public:
     void setZeroes(vector<vector<int>>& a) {
         int n = a.size();
         int m = a[0].size();
-        set<int>s1;
-        set<int>s2;
-        vector<int>a1;
-        vector<int>a2;
+        // set<int>s1;
+        // set<int>s2;
+        // vector<int>a1;
+        // vector<int>a2;
+        int p1=0,p2=0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 if(a[i][j]==0) {
-                    s1.insert(i);
-                    s2.insert(j);
+                    // s1.insert(i);
+                    // s2.insert(j);
+                    if(i==0) p1=1;
+                    if(j==0) p2=1;
+                    a[i][0] = 0;
+                    a[0][j] = 0;
                 }
             }
         }
-        for(auto x:s1) {a1.push_back(x);}
-        for(auto x:s2) {a2.push_back(x);}
+//         for(auto x:s1) {a1.push_back(x);}
+//         for(auto x:s2) {a2.push_back(x);}
         
-        for(int i=0;i<a1.size();i++) {
-            for(int j=0;j<m;j++) {
-                a[a1[i]][j]=0;
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<m;j++) {
+                if(a[i][0]==0 || a[0][j]==0) {
+                    a[i][j]=0;
+                }
             }
         }
-        for(int i=0;i<a2.size();i++) {
-            for(int j=0;j<n;j++) {
-                a[j][a2[i]]=0;
-            }
+        if(p1==1) {
+            for(int i=0;i<m;i++) a[0][i]=0;
         }
-        a1.clear();
-        a2.clear();
-        s1.clear();
-        s2.clear();
+        if(p2==1) {
+            for(int i=0;i<n;i++) a[i][0]=0;
+        }
         // return a;
     }
 };
