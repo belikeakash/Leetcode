7af2bfa45227a3ef6db78a62ac2a2547fbac5a5f
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-22 22:06:22 +0530

    Time: 19 ms (69.54%), Space: 13.3 MB (18.74%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        set<int>s1;
+        set<int>s2;
+        vector<int>a1;
+        vector<int>a2;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(a[i][j]==0) {
+                    s1.insert(i);
+                    s2.insert(j);
+                }
+            }
+        }
+        for(auto x:s1) a1.push_back(x);
+        for(auto x:s2) a2.push_back(x);
+        
+        for(int i=0;i<a1.size();i++) {
+            for(int j=0;j<m;j++) {
+                a[a1[i]][j]=0;
+            }
+        }
+        for(int i=0;i<a2.size();i++) {
+            for(int j=0;j<n;j++) {
+                a[j][a2[i]]=0;
+            }
+        }
+        // return a;
+    }
+};
