2fdf41e433cb7e8a49005cc71ee7eaa1551b24de
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-23 12:20:42 +0530

    Time: 36 ms (77.30%), Space: 7.1 MB (92.68%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,41 +2,33 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        string t = s;
-        reverse(t.begin(),t.end());
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int g=0;g<n;g++) {
-            for(int i=0,j=g;j<n;i++,j++) {
-                if(g==0) {
-                    dp[i][j]=1;
+        int l=0,r=0,st=0,en=0;
+        for(int i=1;i<n;i++) {
+            l = i-1;
+            r = i;
+            while(l>=0 && r<n && s[l]==s[r]) {
+                if(r-l > en-st) {
+                    st = l;
+                    en = r;
                 }
-                else if(g==1) {
-                    if(s[i]==s[j]) dp[i][j] = 1;
-                    else dp[i][j] = 0;
+                l--;
+                r++;
+            }
+            l = i-1;
+            r = i+1;
+            while(l>=0 && r<n && s[l]==s[r]) {
+                if(r-l > en-st) {
+                    st = l;
+                    en = r;
                 }
-                else {
-                    if(s[i]==s[j] && dp[i+1][j-1]==1) dp[i][j] = 1;
-                    else dp[i][j] = 0;
-                }
+                l--;
+                r++;
             }
         }
-        string an = "";
-        int l=0,r=0,ans=1;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(dp[i][j]==1) {
-                    if(j-i+1>ans) {
-                        ans=j-i;
-                        l = i;
-                        r = j;
-                        cout<<l<<" "<<r<<endl;
-                    }
-                }
-            }
+        string ans = "";
+        for(int i=st;i<=en;i++) {
+            ans+=s[i];
         }
-        // cout<<ans<<endl;
-        cout<<endl;
-        for(int i=l;i<=r;i++) an+=s[i];
-        return an;
+        return ans;
     }
 };
