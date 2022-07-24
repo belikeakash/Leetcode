3c097a302c07bd5dac73db2c0d2f97441cf860a1
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-24 16:04:00 +0530

    Time: 80 ms (12.96%), Space: 71.5 MB (50.27%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,61 +10,22 @@
  */
 class Solution {
 public:
-    ListNode* func(ListNode* l1, ListNode* l2) {
-        int c = 0;
-        auto temp1 = l1;
-        auto temp2 = l2;
-        ListNode* p = NULL;
-        // cout<<temp1->val;
-        while(temp1!=NULL && temp2!=NULL) {
-            temp1->val = temp1->val + temp2->val + c;
-            
-            c = temp1->val / 10;
-            temp1->val = temp1->val % 10;
-            cout<<temp1->val<<" ";
-            p = temp1;
-            temp1=temp1->next;
-            temp2=temp2->next;
-        }
-        
-        while(temp1!=NULL) {
-            temp1->val = temp1->val + c;
-            c = temp1->val/10;
-            temp1->val = temp1->val % 10;
-              cout<<temp1->val<<" ";
-            p = temp1;
-            temp1=temp1->next;
-            
-        }
-        cout<<c<<" ";
-        if(temp1==NULL && c>0) {
-            ListNode* x = new ListNode(c);
-            p->next = x;
-            x->next = NULL;
-        }
-        return l1;
-    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int n1 = 0,n2=0;
-        
-        auto temp1 = l1;
-        auto temp2 = l2;
-        
-        while(temp1) {
-            n1++;
-            temp1=temp1->next;
+        int c = 0;
+        auto t1 = l1;
+        auto t2 = l2;
+        ListNode* t3 = new ListNode(0); 
+        auto p = t3;
+        while(c || l1 || l2) {
+            int x = c + (l1?l1->val : 0) + (l2?l2->val : 0);
+            c = x/10;
+            x = x%10;
+            
+            t3->next = new ListNode(x);
+            t3 = t3->next;
+            if(l1) l1=l1->next;
+            if(l2) l2=l2->next;
         }
-        while(temp2) {
-            n2++;
-            temp2=temp2->next;
-        }
-        
-        if(n1>=n2) {
-            return func(l1,l2);
-        }
-        else {
-            return func(l2,l1);
-        }
-        // return l1;
+        return p->next;
     }
 };
