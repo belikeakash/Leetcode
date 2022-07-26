fcbf2c8fd2458f2f055b1414d0b5afdd900e5ef9
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-26 21:18:34 +0530

    Time: 1252 ms (5.03%), Space: 232.7 MB (14.29%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    map<pair<int,int>,bool>m;
+    vector<vector<int>>m;
     bool func(int x, int y, vector<int>&a) {
-        if(y==0) return 0;
-        if(x+y==a[a.size()-1]) return 1;
-        if(x+y>a[a.size()-1]) return 0;
-        if(m.count({x,y})>0) return m[{x,y}];
+        if(y<=0) return 0;
+        if(a[x]+y==a[a.size()-1]) return 1;
+        if(a[x]+y>a[a.size()-1]) return 0;
+        if(m[x][y]!=-1) return m[x][y];
         bool ans = 0;
         for(int i=0;i<a.size();i++) {
-            if(x+y==a[i]) {
-                ans = ans || func(a[i],y-1,a) || func(a[i],y,a) || func(a[i],y+1,a);
+            if(a[x]+y==a[i]) {
+                ans = ans || func(i,y-1,a) || func(i,y,a) || func(i,y+1,a);
             }
         }
-        return m[{x,y}] = ans;
+        return m[x][y] = ans;
     }
     bool canCross(vector<int>& a) {
+        m.resize(a.size()+2,vector<int>(a.size()+2,-1));
         return func(a[0],1,a);
     }
 };
