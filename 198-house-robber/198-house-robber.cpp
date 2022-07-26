99fdfb77a2396c5ea2a418ce2956b41bae8ca8c4
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-26 21:47:19 +0530

    Time: 4 ms (35.28%), Space: 7.8 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    // int ans = 0;
-    vector<int>dp;
-    int func(int pre, int cur, vector<int>&a) {
-        if(cur>a.size()-1) {
-            return 0;
-        }
-        if(dp[cur]!=-1) return dp[cur];
-        int ans = 0;
-        if(cur-pre>1) {
-            ans = max(a[cur] + func(cur,cur+2,a),func(pre,cur+1,a));
-        }
-        return dp[cur] = ans;
-    }
+
     int rob(vector<int>& a) {
-        dp.resize(a.size()+1,-1);
-        return func(-2,0,a);
-        // return ans;
+        int n = a.size();
+        vector<int>dp(n+2,0);
+        for(int i=2;i<n+2;i++) {
+            dp[i] = max(dp[i-1],a[i-2]+dp[i-2]);
+        }
+        return dp[n+1];
     }
 };
