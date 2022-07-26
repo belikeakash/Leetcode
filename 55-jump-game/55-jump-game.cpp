2247f99ecf6164e978fdbc724aedcfa90d18af5f
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-26 16:46:59 +0530

    Time: 82 ms (77.70%), Space: 48.3 MB (93.28%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    vector<int>v;
-    int func(vector<int>&a, int c) {
-        if(c>=a.size()-1) return 1;
-        // if(a[c]==0) return 0;
-        if(v[c]!=-1) {return v[c];}
-        
-        for(int i=c+1;i<=c+a[c];i++) {
-            if(func(a,i)==1) return v[c] = 1;
-        }
-        return v[c] = 0;
-    }
+   
     bool canJump(vector<int>& a) {
-        
         int n = a.size();
-        if(n==1) return 1;
-        v.resize(n,-1);
-        bool ans = func(a,0);
-        return ans;
+        int c = 0;
+        for(int i=n-2;i>=0;i--) {
+            c++;
+            if(c<=a[i]) c = 0;
+            
+        }
+        if(c==0) return 1;
+        return 0;
     }
 };
