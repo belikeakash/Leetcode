084dab5126856b321f89bcb1bc67dc30f51aad12
Akash <76730100+belikeakash@users.noreply.github.com>
2022-07-28 12:26:13 +0530

    Time: 15 ms (76.42%), Space: 14.8 MB (29.39%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int ans = 0;
+    vector<vector<int>>dp;
     int func(int i, vector<int>&a, int x, int sum) {
+        if(sum>x) return 0;
         if(i==a.size()) {
             if(sum==x) return 1;
             else return 0;
         }
-        return func(i+1,a,x,sum+a[i]) + func(i+1,a,x,sum);
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        return dp[i][sum] = func(i+1,a,x,sum+a[i]) + func(i+1,a,x,sum);
     }
     int findTargetSumWays(vector<int>& a, int target) {
         int sum = 0;
         for(auto x:a) sum+=x;
         int x = sum-target;
         cout<<x<<" ";
-        if(x%2!=0) return 0;
+        if(x%2!=0 || x<0) return 0;
+        dp.resize(a.size()+1,vector<int>(x+1,-1));
         return func(0,a,x/2,0);
         // return ans;
     }
