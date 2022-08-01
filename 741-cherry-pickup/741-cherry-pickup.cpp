b6c9901de388880c7ca8cce18350ba4523251183
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-01 12:16:31 +0530

    Time: 1933 ms (5.03%), Space: 136.4 MB (14.73%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int ans = 0;
     unordered_map<string,int>m;
-    int func(int i1, int j1, int i2, int j2, int n, vector<vector<int>>&a) {
+    int func(int i1, int j1, int i2, int n, vector<vector<int>>&a) {
+        int j2 = i1+j1-i2;
         if(i1>=n || i2>=n || j1>=n || j2>=n || a[i1][j1]==-1 || a[i2][j2]==-1) return INT_MIN;
         if(i1==n-1 && j1==n-1) {
             return a[i1][i2];
@@ -16,10 +17,10 @@ public:
         else {
             c += a[i1][j1] + a[i2][j2];
         }
-        int p1 = func(i1+1,j1,i2+1,j2,n,a);
-        int p2 = func(i1,j1+1,i2+1,j2,n,a);
-        int p3 = func(i1+1,j1,i2,j2+1,n,a);
-        int p4 = func(i1,j1+1,i2,j2+1,n,a);
+        int p1 = func(i1+1,j1,i2+1,n,a);
+        int p2 = func(i1,j1+1,i2+1,n,a);
+        int p3 = func(i1+1,j1,i2,n,a);
+        int p4 = func(i1,j1+1,i2,n,a);
         // cout<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<c<<endl;
         c += max({p1,p2,p3,p4});
         cout<<c<<" ";
@@ -29,6 +30,6 @@ public:
     int cherryPickup(vector<vector<int>>& a) {
         int n = a.size();
         
-        return max(0,func(0,0,0,0,n,a));
+        return max(0,func(0,0,0,n,a));
     }
 };
