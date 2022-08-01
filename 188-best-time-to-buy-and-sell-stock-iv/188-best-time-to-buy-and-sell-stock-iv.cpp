c20655e64cfcd330889b0c0087d084f2447a267b
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-01 14:15:33 +0530

    Time: 27 ms (34.12%), Space: 12.7 MB (38.57%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int func(int s, int k, vector<int>&a,vector<vector<int>>&dp) {
-        if(s==a.size()) return 0;
-        if(k==0) return 0;
-        if(dp[s][k]!=-1) return dp[s][k];
-        int ans1 = func(s+1,k,a,dp);
-        
-        bool x = (k%2==0);
-        int ans2 = 0;
-        if(x==true) {
-            ans2 = -a[s] + func(s+1,k-1,a,dp);
+    int func(int i, int t, vector<int>&a, vector<vector<int>>&dp) {
+        if(i==a.size() || t==0) return 0;
+        if(dp[i][t]!=-1)return dp[i][t];
+        if(t%2==0) {
+            int p1 = -a[i] + func(i+1,t-1,a,dp);
+            int p2 = func(i+1,t,a,dp);
+            return dp[i][t] = max(p1,p2);
         }
         else {
-            ans2 = a[s] + func(s+1,k-1,a,dp);
+            int p3 = a[i] + func(i+1,t-1,a,dp);
+            int p4 = func(i+1,t,a,dp);
+            return dp[i][t] = max(p3,p4);
         }
-        cout<<ans1<<" "<<ans2<<endl;
-        return dp[s][k] = max(ans1,ans2);
     }
     int maxProfit(int k, vector<int>& a) {
         vector<vector<int>>dp(a.size()+1,vector<int>(2*k +2,-1));
