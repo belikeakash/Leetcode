764209b0d679d9884a00d7b8fe15c6a60780d991
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-01 17:23:54 +0530

    Time: 625 ms (39.45%), Space: 135.2 MB (41.00%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+    
+    
     int maxProfit(vector<int>& a) {
-        int n = a.size();
-        vector<int>a1(n,0),a2(n,0);
-        int mini = a[0];
-        for(int i=1;i<n;i++) {
-            if(a[i]<mini) mini = a[i];
-            a1[i] = max(a1[i-1],a[i]-mini);
-        }
-        for(auto x:a1) cout<<x<<" ";
-        int maxi = a[n-1];
-        for(int i=n-2;i>=0;i--) {
-            if(a[i]>maxi) maxi = a[i];
-            a2[i] = max(a2[i+1],maxi - a[i]);
-        }
         int ans = 0;
-        for(int i=0;i<n;i++) {
-            ans = max(ans,a1[i]+a2[i]);
+        int n = a.size();
+        dp.resize(a.size()+1,vector<int>(5,0));
+        for(int i=n-1;i>=0;i--) {
+            for(int j=0;j<4;j++) {
+                if(j%2==0) {
+                    dp[i][j] = max(-a[i]+dp[i+1][j+1],dp[i+1][j]);
+                }
+                else {
+                    dp[i][j] = max(a[i]+dp[i+1][j+1],dp[i+1][j]);
+                }
+            }
         }
-        return ans;
+        return dp[0][0];
     }
 };
