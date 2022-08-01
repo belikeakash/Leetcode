068ddec05cb18769816526bea4f92d01ba9da435
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-01 12:17:20 +0530

    Time: 1320 ms (5.03%), Space: 136.6 MB (14.57%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -5,12 +5,13 @@ public:
     int func(int i1, int j1, int i2, int n, vector<vector<int>>&a) {
         int j2 = i1+j1-i2;
         if(i1>=n || i2>=n || j1>=n || j2>=n || a[i1][j1]==-1 || a[i2][j2]==-1) return INT_MIN;
-        if(i1==n-1 && j1==n-1) {
-            return a[i1][i2];
-        }
         string s = to_string(i1)+"*"+to_string(i2)+"*"+to_string(j1)+"*"+to_string(j2);
         int c = 0;
         if(m.count(s)) return m[s];
+        if(i1==n-1 && j1==n-1) {
+            return a[i1][i2];
+        }
+        
         if(i1==i2 && j1==j2) {
             c += a[i1][j1];
         }
