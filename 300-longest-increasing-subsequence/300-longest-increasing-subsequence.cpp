3447aa4012bda79360b57aecd9932b1c1cdb7c5f
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-02 12:58:08 +0530

    Time: 1177 ms (13.59%), Space: 292.5 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a) {
-        vector<int>v;
-        v.push_back(a[0]);
-        for(int i=1;i<a.size();i++) {
-            if(a[i]<=v.back()){
-                auto j = lower_bound(v.begin(),v.end(),a[i]) - v.begin();
-                v[j] = a[i];
-            }
-            else v.push_back(a[i]);
-            
+    vector<vector<int>>dp;
+    int func(int pre, int cur, vector<int>&a) {
+        if(cur==a.size()) return 0;
+        if(dp[pre+1][cur+1]!=-1) return dp[pre+1][cur+1];
+        if(pre==-1 || a[cur]>a[pre]) {
+            return dp[pre+1][cur+1] = max(1 + func(cur,cur+1,a), func(pre,cur+1,a)) ;
         }
-        return v.size();
+        else {
+            return dp[pre+1][cur+1] = func(pre,cur+1,a);
+        }
+    }
+    int lengthOfLIS(vector<int>& a) {
+        dp.resize(a.size()+1,vector<int>(a.size()+1,-1));
+        return func(-1,0,a);
     }
 };
