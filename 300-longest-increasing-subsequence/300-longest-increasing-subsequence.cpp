a3de51197fcb23baf14677a4038edc339f609b7e
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-02 13:29:05 +0530

    Time: 768 ms (19.21%), Space: 292.3 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -8,11 +8,29 @@ public:
             return dp[pre+1][cur+1] = max(1 + func(cur,cur+1,a), func(pre,cur+1,a)) ;
         }
         else {
-            return dp[pre+1][cur+1] = func(pre,cur+1,a);
+            return dp[pre+1][cur] = func(pre,cur+1,a);
         }
     }
     int lengthOfLIS(vector<int>& a) {
-        dp.resize(a.size()+1,vector<int>(a.size()+1,-1));
-        return func(-1,0,a);
+        int n = a.size();
+        dp.resize(a.size()+1,vector<int>(a.size()+1,0));
+        // return func(-1,0,a);
+        for(int i=n-1;i>=0;i--) {
+            for(int j=i-1;j>=-1;j--) {
+                if(j==-1 || a[i]>a[j]) {
+                    dp[i][j+1] = max(1+dp[i+1][i+1],dp[i+1][j+1]);
+                }
+                else {
+                    dp[i][j+1] = dp[i+1][j+1];
+                }
+            }
+        }
+        // for(int i=0;i<=n;i++) {
+        //     for(int j=0;j<=n;j++) {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[0][0];
     }
 };
