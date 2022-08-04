d8a956a8be333f9ceaa93b3b2f5ac7205f71667e
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-04 11:21:29 +0530

    Time: 38 ms (51.90%), Space: 16.6 MB (53.73%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    vector<int>dp;
-    int func(int x, vector<int>&a) {
-        if(x>=a.size()-1) return 0;
-        if(dp[x]!=-1) return dp[x];
-        int ans = 100007;
-        for(int i=x+1;i<=x+a[x];i++) {
-            ans = min(ans,1+func(i,a));
-        }
-        return dp[x] = ans;
-    }
+    
     int jump(vector<int>& a) {
-    //     if(a.size()==1) return 0;
         int n = a.size();
-        dp.resize(a.size()+1,100009);
-        dp[0] = 0;
-        for(int i=0;i<n;i++) {
-            for(int j=i+1;j<=i+a[i] && j<n;j++) {
-                dp[j] = min(dp[j],dp[i]+1);
+        int s = 0,e=0,ans=0,maxi=0;
+        
+        while(e<n-1) {
+            ans++;
+            for(int i=s;i<=e;i++) {
+                maxi = max(maxi,i+a[i]);
+                if(maxi>=n-1) return ans;
             }
+            s = e+1;
+            e = maxi;
         }
-        return dp[n-1];
-        // return func(0,a);
+        return ans;
     }
 };
