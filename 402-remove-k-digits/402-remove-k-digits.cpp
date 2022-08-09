7da3b257e6ba3b3897bf5c8217ccd626267ab5b5
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-09 18:20:53 +0530

    Time: 26 ms (30.31%), Space: 9 MB (23.71%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -2,22 +2,34 @@ class Solution {
 public:
     string removeKdigits(string s, int k) {
         int n = s.size();
-        stack<char>st;
-        for(auto x:s) {
-            while(!st.empty() && k>0 && x<st.top()) {
-                st.pop();
-                k--;
+        if(k>=n) return "0";
+        else {
+            stack<char>st;
+            for(auto x:s) {
+                while(!st.empty() && k>0 && st.top()>x) {
+                    st.pop();
+                    k--;
+                }
+                st.push(x);
             }
-            if(x!='0' || !st.empty()) st.push(x);
+             
+            string ans = "";
+            while(!st.empty()) {
+                ans+=st.top();
+                st.pop();
+            }
+            cout<<ans<<endl;
+            reverse(ans.begin(),ans.end());
+            while(k>0) {ans.pop_back(); k--;}
+            reverse(ans.begin(),ans.end());
+            int p = ans.size();
+            string q = ans;
+            for(int i=p-1;i>0;i--) {
+                if(ans[i]=='0') q.pop_back();
+                else break;
+            }
+            reverse(q.begin(),q.end());
+            return q;
         }
-        while(k-->0 && !st.empty()) st.pop();
-        string ans="";
-        if(st.empty()) return "0";
-        while(!st.empty()) {
-            ans+=st.top();
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
     }
 };
