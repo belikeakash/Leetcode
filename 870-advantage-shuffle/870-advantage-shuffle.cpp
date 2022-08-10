11d17815e9dc83d1d0e4ed6385fdcb86f86fdf3f
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-10 13:38:06 +0530

    Time: 476 ms (12.48%), Space: 62.7 MB (84.82%) - LeetHub

---
+++ 870-advantage-shuffle/870-advantage-shuffle.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        vector<int>ans(n,-1);
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++) {
+            v.push_back({b[i],i});
+        }
+        map<int,int>m;
+        sort(v.begin(),v.end());
+        // for(int i=0;i<n;i++) v[i].first = i;
+        sort(a.begin(),a.end());
+        int p1 = 0;
+        vector<int>c;
+        int i = 0;
+        while(p1<n) {
+            cout<<p1<<" ";
+            if(a[p1]<=v[i].first) {
+                c.push_back(a[p1]);
+                p1++;
+            }
+            else {
+                ans[v[i].second] = a[p1];
+                i++;
+                p1++;
+            }
+        }
+        cout<<endl;
+        int j = 0;;
+        for(int i=0;i<n;i++) {
+            if(ans[i]==-1) {
+                ans[i] = c[j];
+                j++;
+            }
+        }
+        return ans;
+    }
+};
