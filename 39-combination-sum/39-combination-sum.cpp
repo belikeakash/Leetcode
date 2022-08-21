a287fff8cf57dd1220751f5307f99c4048c7f970
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-21 21:06:15 +0530

    Time: 8 ms (81.48%), Space: 11.4 MB (47.59%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<vector<int>>ans;
     vector<int>v;
-    void func(int i, vector<int>&a, int sum) {
+    int func(int i, vector<int>&a, int sum) {
         
-        if(sum==0) {ans.push_back(v); return;}
-        if(i>=a.size() || i<0) return ;
-        if(sum<0) return ;
-        func(i+1,a,sum);
-        v.push_back(a[i]);
-        func(i,a,sum-a[i]);
-        v.pop_back();
-        // return func(i+1,a,sum);
+        if(sum==0) {ans.push_back(v); return 1;}
+        if(i>=a.size() || i<0) return 0;
+        if(sum<0) return 0;
+        if(a[i]<=sum) {
+            v.push_back(a[i]);
+            func(i,a,sum-a[i]); v.pop_back();
+        }
+        return func(i+1,a,sum);
         
     }
     vector<vector<int>> combinationSum(vector<int>& a, int target) {
