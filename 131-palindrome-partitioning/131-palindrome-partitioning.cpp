38edfabb2f63eedb8a0d69a10a7d7116ab0b489f
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-22 17:50:11 +0530

    Time: 387 ms (15.43%), Space: 80 MB (32.37%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -9,25 +9,28 @@ public:
     vector<string>v;
     void func(int i, string s) {
         if(i==s.size()) {
+            for(auto x:v) {
+                if(!isPal(x)) return;
+            }
             ans.push_back(v);
             return ;
         }
         string p = "";
         for(int j=i;j<s.size();j++) {
             p+=s[j];
-            if(isPal(p)) v.push_back(p);
+            v.push_back(p);
             func(j+1,s);
-            if(isPal(p)) v.pop_back();
+            v.pop_back();
         }
     }
     vector<vector<string>> partition(string s) {
         func(0,s);
-        vector<vector<string>>res;
-        for(auto x:ans) {
-            string u = "";
-            for(auto y:x) u+=y;
-            if(u.size()==s.size()) res.push_back(x);
-        }
-        return res;
+        // vector<vector<string>>res;
+        // for(auto x:ans) {
+        //     string u = "";
+        //     for(auto y:x) u+=y;
+        //     if(u.size()==s.size()) res.push_back(x);
+        // }
+        return ans;
     }
 };
