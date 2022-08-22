880d8c18fc484cee7366be2548cce087629d3937
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-23 00:18:37 +0530

    Time: 145 ms (31.92%), Space: 10.4 MB (15.22%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -3,24 +3,21 @@ public:
     vector<vector<int>>dp;
     int func(int i, int j, vector<int>&a) {
         
-        if(i+1==j) return 0;
-        else if(dp[i][j]!=-1) return dp[i][j];
-        else {
-           int ans = INT_MAX;
-        for(int k=i+1;k<j;k++) {
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans = INT_MAX;
+        for(int k=i;k<=j;k++) {
             // cout<<i<<" "<<j<<endl;
-            int cost = a[j]-a[i] + func(i,k,a) + func(k,j,a);
+            int cost = a[j+1]-a[i-1] + func(i,k-1,a) + func(k+1,j,a);
             ans = min(ans,cost);
         }
-        return dp[i][j] = ans; 
-        }
-        
+        return dp[i][j] = ans;
     }
     int minCost(int n, vector<int>& a) {
         a.push_back(0);
         a.push_back(n);
         sort(a.begin(),a.end());
-        dp.resize(102,vector<int>(102,-1));
-        return func(0,a.size()-1,a);
+        dp.resize(a.size()+1,vector<int>(a.size()+1,-1));
+        return func(1,a.size()-2,a);
     }
 };
