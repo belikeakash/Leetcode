8106eb20661bfff83ee1d027585e2ad913b6f85a
Akash <76730100+belikeakash@users.noreply.github.com>
2022-08-31 14:39:44 +0530

    Time: 8 ms (40.21%), Space: 11.3 MB (28.01%) - LeetHub

--- 969-pancake-sorting/969-pancake-sorting.cpp
+++ 969-pancake-sorting/969-pancake-sorting.cpp
@@ -11,10 +11,10 @@ public:
             for(int i=0;i<a.size();i++) {
                 if(a[i]!=b[i]) flag=1;
             }
-            for(int i=0;i<a.size();i++) {
-                cout<<a[i]<<" ";
-            }
-            cout<<endl;
+            // for(int i=0;i<a.size();i++) {
+            //     cout<<a[i]<<" ";
+            // }
+            // cout<<endl;
             if(flag==0) break;
             int i = -1;
             for(i=0;i<j;i++) {
@@ -23,10 +23,10 @@ public:
             for(int p=0;p<=i/2;p++) {
                 swap(a[p],a[i-p]);
             }
-            for(int i=0;i<a.size();i++) {
-                cout<<a[i]<<" ";
-            }
-            cout<<endl;
+            // for(int i=0;i<a.size();i++) {
+            //     cout<<a[i]<<" ";
+            // }
+            // cout<<endl;
             i++;
             ans.push_back(i);
             ans.push_back(j);
