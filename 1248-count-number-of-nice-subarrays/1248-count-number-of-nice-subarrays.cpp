835def8862b986706ed4885271e57fdcf3e2f931
Akash <76730100+belikeakash@users.noreply.github.com>
2022-09-01 18:09:39 +0530

    Time: 706 ms (5.10%), Space: 73.9 MB (40.21%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,18 +2,45 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& a, int k) {
         int n = a.size();
-        unordered_map<int,int>m;
-        for(auto &x:a) {
-            if(x%2==0) x = 0;
-            else x = 1;
-        }
-        int ans = 0;
-        int sum = 0;
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        int c = 0;
         for(int i=0;i<n;i++) {
-            sum+=a[i];
-            ans+=m[sum-k];
-            m[sum]++;
-            if(sum==k) ans++;
+            if(a[i]%2==0) c++;
+            else {right[i]=c; c=0;}
+        }
+        c = 0;
+        for(int i=n-1;i>=0;i--) {
+            if(a[i]%2==0) c++;
+            else {left[i]=c; c=0;}
+        }
+        // for(int i=0;i<n;i++) cout<<left[i]<<" ";
+        // cout<<endl;
+        // for(int i=0;i<n;i++) cout<<right[i]<<" ";
+        
+        int i = 0;
+        int j = 0;
+        int x = 0;
+        int ans = 0;
+        while(j<n) {
+            if(j<n && a[j]%2!=0) x++;
+            if(x>k) {
+                while(x>k) {
+                    if(i<n && a[i]%2!=0) x--;
+                    i++;
+                }
+            }
+            else if(x==k) {
+                while(i<n && a[i]%2==0) {
+                    i++;
+                }
+                cout<<ans<<" ";
+                ans+=(right[i]+1)*(left[j]+1);
+                if(a[i]%2!=0) x--;
+                i++;
+            }
+            
+            j++;
         }
         return ans;
     }
