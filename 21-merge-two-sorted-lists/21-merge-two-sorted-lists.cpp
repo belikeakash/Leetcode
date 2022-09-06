93915adc2c30d1f3520126b7648374857314ccdf
Akash <76730100+belikeakash@users.noreply.github.com>
2022-09-06 12:07:09 +0530

    Time: 15 ms (37.62%), Space: 15 MB (7.97%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,44 +10,33 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* h1, ListNode* h2) {
-        ListNode* curr1 = h1;
-        ListNode* next1 = curr1->next;
-        ListNode* curr2 = h2;
-        ListNode* next2 = curr2->next;
-        if(next1==NULL) {
-            curr1->next = curr2;
-            return h1;
-        }
-        
-        while(next1!=NULL && curr2!=NULL) {
-            cout<<next1->val<<" ";
-            if(curr2->val >= curr1->val && curr2->val <=next1->val) {
-                curr1->next = curr2;
-                next2 = curr2->next;
-                curr2->next = next1;
-                curr1 = curr2;
-                curr2 = next2;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* dumm = dummy;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        while(temp1!=NULL && temp2!=NULL) {
+            if(temp1->val < temp2->val) {
+                dumm->next = new ListNode(temp1->val);
+                dumm=dumm->next;
+                temp1=temp1->next;
             }
             else {
-                curr1 = next1;
-                next1 = next1->next;
-                if(next1==NULL) {
-                    curr1->next = curr2;
-                    return h1;
-                }
+                dumm->next = new ListNode(temp2->val);
+                dumm=dumm->next;
+                temp2=temp2->next;
             }
         }
-        return h1;
-    }
-    ListNode* mergeTwoLists(ListNode* h1, ListNode* h2) {
-        if(h1==NULL) return h2;
-        if(h2==NULL) return h1;
-        if(h1->val<=h2->val) {
-            return solve(h1,h2);
+        while(temp1) {
+            dumm->next = new ListNode(temp1->val);
+            dumm=dumm->next;
+            temp1=temp1->next;
         }
-        else {
-            return solve(h2,h1);
+        while(temp2) {
+            dumm->next = new ListNode(temp2->val);
+            dumm=dumm->next;
+            temp2=temp2->next;
         }
+        return dummy->next;
     }
 };
