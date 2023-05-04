2cc08c8f83dbf910091c23d98888512aabb2e56c
Akash <76730100+belikeakash@users.noreply.github.com>
2023-05-04 17:45:54 +0530

    Time: 45 ms (79.48%), Space: 14.4 MB (99.33%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,46 +8,39 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *h1, ListNode *h2) {
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
         int n1 = 0;
         int n2 = 0;
-        ListNode* temp = h1;
-        while(temp) {
+        ListNode* h1 = head1;
+        while(h1) {
+            h1=h1->next;
             n1++;
-            temp = temp->next;
         }
-        temp = h2;
-        while(temp) {
+        ListNode* h2 = head2;
+        while(h2) {
+            h2=h2->next;
             n2++;
-            temp = temp->next;
         }
-        if(n1>n2) {
-            int c = n1-n2;
-            ListNode* u1 = h1;
-            ListNode* u2 = h2;
-            while(c--) {
-                u1 = u1->next;
-            }
-            while(u1!=u2) {
-                u1=u1->next;
-                u2=u2->next;
-            }
-            
-            return u1;
+        int k;
+        if(n1>n2){
+            h1 = head1;
+            h2 = head2;
+            k = n1-n2;
         }
         else {
-            int c = n2-n1;
-            ListNode* u1 = h1;
-            ListNode* u2 = h2;
-            while(c--) {
-                u2 = u2->next;
-            }
-            while(u1!=u2) {
-                u1=u1->next;
-                u2=u2->next;
-            }
-            
-            return u1;
+            h1 = head2;
+            h2 = head1;
+            k = n2-n1;
         }
+        
+        cout<<k<<endl;
+        while(k--) {
+            h1=h1->next;
+        }
+        while(h1!=h2) {
+            h1=h1->next;
+            h2=h2->next;
+        }
+        return h1;
     }
 };
