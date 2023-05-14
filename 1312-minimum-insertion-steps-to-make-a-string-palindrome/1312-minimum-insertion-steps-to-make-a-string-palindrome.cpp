4ec20c7efea03497c563429796c5477801997bbb
Akash <76730100+belikeakash@users.noreply.github.com>
2023-05-14 21:40:49 +0530

    Time: 61 ms (55.58%), Space: 28.5 MB (19.66%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[510][510];
+    vector<vector<int>>dp;
     int mininsertions(string&s, int si, int ei){
         if(dp[si][ei]!=-1)return dp[si][ei];
         if(si>=ei)return 0;
@@ -10,7 +10,7 @@ public:
         return dp[si][ei]=1+min(mininsertions(s,si+1,ei),mininsertions(s,si,ei-1));
     }
     int minInsertions(string s) {
-        memset(dp,-1,sizeof(dp));
+        dp.resize(s.size()+1, vector<int>(s.size()+1,-1));
         return mininsertions(s,0,s.size()-1);
     }
 };
