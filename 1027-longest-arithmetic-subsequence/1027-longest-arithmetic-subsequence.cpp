ee7c5b84bc0d9df450de7c49190b7b8e07039a05
Akash <76730100+belikeakash@users.noreply.github.com>
2023-06-24 17:21:48 +0530

    Time: 1392 ms (51.65%), Space: 15.5 MB (91.52%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,39 +1,34 @@
 class Solution {
-    int dp[1001][1002];
 public:
-    int solve(int idx,int prev,int n,unordered_map<int,vector<int>> &mp,vector<int> &nums) {
-        if(idx >= n) {return 0;}
-        if(dp[idx][prev+1]!=-1) {return dp[idx][prev+1];}
-
-        int ans = 1;
+    int dp[1001][1002];
+    unordered_map<int,vector<int>>mp;
+    int func(int i, int prev, vector<int>&a) {
+        if(i>=a.size()) return 0;
         
-        if(prev == -1) {
-            for(int i=idx+1;i<n;i++) {
-                ans = max(ans, 1 + solve(i,idx,n,mp,nums));
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        int maxi = 1;
+        if(prev==-1) {
+            for(int j=i+1;j<a.size();j++) {
+                maxi = max(maxi, 1+func(j, i, a));
             }
-        }else {
-            int next = nums[idx]+(nums[idx]-nums[prev]);
-            if(mp.find(next)==mp.end()) {return 1;}
-            int nextIdx = lower_bound(mp[next].begin(),mp[next].end(), idx+1)-mp[next].begin();
-            //cout<<next<<" "<<idx<<" "<<nextIdx<<endl;
-            if(nextIdx==mp[next].size()) {return 1;}
-            ans = max(ans, 1+ solve(mp[next][nextIdx], idx,n, mp,nums));
-        }   
-        return dp[idx][prev+1]=ans;
-    }
-
-    int longestArithSeqLength(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        unordered_map<int,vector<int>> mp;
-        int n = nums.size();
-
-        for(int i=0;i<nums.size();i++) {
-            mp[nums[i]].push_back(i);
         }
-
+        else {
+            int next = a[i] + a[i]-a[prev];
+            if(!mp.count(next)) return 1;
+            int pos = lower_bound(mp[next].begin(), mp[next].end(), i+1) - mp[next].begin();
+            if(pos==mp[next].size()) return 1;
+            maxi = max(maxi, 1+func(mp[next][pos],i, a)) ;
+        }
+        return dp[i][prev+1] = maxi;
+    }
+    int longestArithSeqLength(vector<int>& a) {
+        for(int i=0;i<a.size();i++) {
+            mp[a[i]].push_back(i);
+        }
+        memset(dp,-1,sizeof(dp));
         int ans = 1;
-        for(int i=0;i<nums.size();i++) {
-            ans = max(ans, solve(i,-1,n,mp,nums));
+        for(int i=0;i<a.size();i++) {
+            ans = max(ans, func(i, -1, a));
         }
         return ans;
     }
