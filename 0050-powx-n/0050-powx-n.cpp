41c6df1e0ca2ab5e152f03850c0fbe9a6c689643
Akash <76730100+belikeakash@users.noreply.github.com>
2023-07-26 17:27:25 +0530

    Time: 3 ms (39.94%), Space: 6.1 MB (27.33%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    double func(double x, long long n) {
-        if(n==0) return 1;
-        double ans = func(x,n/2)    ;
-        if(n%2==0) return ans*ans;
-        else return x*ans*ans;
-    }
     double myPow(double x, long long n) {
-        if(n<0) {
-            n = -n;
-            x = 1/x;
-        }
-        return func(x,n);
+        if(n<0) return myPow(1/x, -n);
+        if(n==0) return 1;
+        double ans = myPow(x,n/2);
+        if(n%2!=0) return x*ans*ans;
+        return ans*ans;
     }
 };
-
-
