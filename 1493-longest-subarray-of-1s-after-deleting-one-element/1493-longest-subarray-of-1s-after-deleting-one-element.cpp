28da6e6ebb17b9c5197a8acb603dc7c2f20aeb54
Akash <76730100+belikeakash@users.noreply.github.com>
2023-07-28 14:14:47 +0530

    Time: 40 ms (85.40%), Space: 50.6 MB (7.11%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& a) {
-        int i = 0, j=0;
-        int c = 0;
-        int ans = 1;
         int n = a.size();
-        while(i<n) {
-            if(a[i]==1) i++;
-            else if(a[i]==0 && c==0) {i++; c++;}
-            else if(a[i]==0 && c==1) {
-                while(c>0) {
-                    if(a[j]==0) c--;
-                    j++;
-                }
-                i++;
-                c++;
+        int i = 0, j=0;
+        int ans = 0;
+        int cnt = 0;
+        // while(j<n) {
+        //     if(a[j]==0) cnt++;
+        //     if(cnt>1) {
+        //         while(cnt>1 && i<j) {
+        //             if(a[i]==0) cnt--;
+        //             i++;
+        //         }
+        //     }
+        //     ans = max(ans, j-i+1);
+        //     j++;
+        // }
+        // return ans;
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        int c = 0;
+        bool flag = 0;
+        for(int i=0;i<n;i++) {
+            if(a[i]==1) c++;
+            else{
+                left[i] = c;
+                c=0;
+                flag=1;
             }
-            ans = max(ans, i-j);
         }
-        return ans-1;
+        c=0;
+        for(int i=n-1;i>=0;i--) {
+            if(a[i]==1) {c++;}
+            else{
+                right[i] = c;
+                c=0;
+                 flag=1;
+            }
+        }
+        for(int i=0;i<n;i++) {
+            ans = max(ans, left[i]+right[i]);
+        }
+        
+        if(!flag) return n-1;
+        return ans;
     }
 };
