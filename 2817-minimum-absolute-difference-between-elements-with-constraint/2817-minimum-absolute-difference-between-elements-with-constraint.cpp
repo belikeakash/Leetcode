75abcf8514cae7fa5d38ce4cecd6aa0b3fa04bd4
Akash <76730100+belikeakash@users.noreply.github.com>
2023-08-13 18:05:32 +0530

    Time: 229 ms (100.00%), Space: 118 MB (20.00%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int minAbsoluteDifference(vector<int>& a, int x) {
-        multiset<int>s;
-        
+        set<int>s;
+        int ans = INT_MAX;
         int n = a.size();
-        for(int i=x;i<n;i++) {
-            s.insert(a[i]);
-        }
-        int mini = INT_MAX;
-        for(int i=0;i<n-x;i++) {
-            auto it = s.lower_bound(a[i]);
-            cout<<*it<<" ";
-            if(it!=s.end()) {
-                mini = min(mini, abs(*it - a[i]));
+        for(int i=n-x-1;i>=0;i--) {
+            s.insert(a[i+x]);
+            auto c = s.lower_bound(a[i]);
+            if(c!=s.end()) {
+                ans = min(ans, abs(a[i]-*c));
             }
-            if(it!=s.begin()) {
-                mini = min(mini, abs(*std::prev(it) - a[i]));
+            if(c!=s.begin()) {
+                ans = min(ans, abs(a[i]-*prev(c)));
             }
-            s.erase(s.find(a[i+x]));
         }
         
-        return mini;
+        return ans;
     }
 };
