d2ba7cd886479339f7d4651bf170c9bbf12e6051
Akash <76730100+belikeakash@users.noreply.github.com>
2023-08-26 13:52:34 +0530

    Time: 468 ms (20.38%), Space: 159.3 MB (6.17%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,14 +1,32 @@
 class Solution {
     vector<vector<int>>dp;
 public:
+    int helper(vector<vector<int>>&a, int idx) {
+        int ans = INT_MAX;
+        
+        int l = idx+1, r = a.size()-1;
+        int x = a[idx][1];
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            if(a[mid][0]>x) {
+                ans = mid;
+                r = mid-1;
+            }
+            else {
+                l = mid+1;
+            }
+        }
+        
+        return ans;
+    }
     int func(int prev, int i, vector<vector<int>>&a) {
         if(i>=a.size()) return 0;
         if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        int idx = helper(a, i);
         if(prev==-1) {
-            return dp[i][prev+1] = max(1+func(i, i+1, a), func(-1, i+1, a));
+            return dp[i][prev+1] = max(1+func(i, idx, a), func(-1, i+1, a));
         }
-        if(a[i][0]>a[prev][1]) return dp[i][prev+1] = max( 1 + func(i, i+1, a), func(prev, i+1, a));
-        else return dp[i][prev+1] = func(prev, i+1, a);
+        return dp[i][prev+1] = max( 1 + func(i, idx, a), func(prev, i+1, a));
     }
     int findLongestChain(vector<vector<int>>& a) {
         sort(a.begin(), a.end());
