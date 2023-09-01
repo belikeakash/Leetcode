cc03af64519c98bf0642f7a89dd6a3c5cb1ebc78
Akash <76730100+belikeakash@users.noreply.github.com>
2023-09-01 13:07:42 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -6,23 +6,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-    int *findTwoElement(int *a, int n) {
-        int*ans = new int[2];
-        
-        for(int i=0;i<n;i++) {
-            //cout<<a[i]<<" ";
-            if(a[abs(a[i])-1]<0) {
-                ans[0] = abs(a[i]);
+    vector<int> findTwoElement(vector<int> a, int n) {
+        vector<int>ans;
+    int sum = 0;
+    for(auto x:a) {
+        sum+=x;
+    }
+    int i = 0;
+    int n1 = -1;
+    // cout<<"a";
+    while(i<n) {
+        //cout<<i<<"->";
+        while(a[i]!=i+1) {
+            if(a[a[i]-1]==a[i]) {
+                n1 = a[i];
+                break;
             }
-            else a[abs(a[i])-1] = -a[abs(a[i])-1];
-        }
-        //cout<<endl;
-        for(int i=0;i<n;i++) {
-            if(a[i]>0) {
-                ans[1] = i+1;
+            else {
+                swap(a[i], a[a[i]-1]);
             }
         }
-        return ans;
+        // if(n1!=-1) break;
+        i++;
+    }
+
+    for(int i=0;i<n;i++) {
+        if(a[i]!=i+1) {
+            ans.push_back(a[i]);
+            ans.push_back(i+1);
+        }
+    }
+    return ans;
     }
 };
 
@@ -34,7 +48,7 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        int a[n];
+        vector<int> a(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
