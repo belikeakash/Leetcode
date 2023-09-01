32a016af929005f0c878ad3b9c9fce0071a5f987
Akash <76730100+belikeakash@users.noreply.github.com>
2023-09-01 18:10:54 +0530

    Time: 5 ms (66.40%), Space: 13.7 MB (66.65%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
-    int ans = INT_MIN;
-    vector<long long>dp;
-    
     int maxProduct(vector<int>& a) {
         int n = a.size();
-        int i = 0;
+        int prod = 0;
         int ans = INT_MIN;
-        while(i<n) {
-            // cout<<"a";
-            int j = i;
-            int neg = 0;
-            while(j<n && a[j]!=0) {
-                if(a[j]<0) neg++;
-                j++;
+        for(int i=0;i<n;i++) {
+            if(a[i]==0) {
+                ans = max({ans, 0, prod});
+                prod = 0;
             }
-            int l = i, r = j-1;
-            int mini = a[l];
-            if(neg%2==0) {
-                for(int k=l+1;k<=r;k++) mini = mini*a[k];
-                ans=max(ans, mini);
+            
+            else {
+                if(prod==0) prod = a[i];
+                else prod = prod*a[i];
+                ans = max(ans, prod);
+            }
+        }
+        prod = 0;
+        for(int i=n-1;i>=0;i--) {
+            if(a[i]==0) {
+                ans = max({ans, 0, prod});
+                prod = 0;
             }
             else {
-                int prod = 1;
-                int mini = INT_MIN;
-                for(int k=l;k<=r;k++) {
-                    prod=prod*a[k];
-                }
-                int m1 = a[l], m2 = INT_MIN;
-                if(a[l]<0) {
-                    if(l<r) m2 = prod/(m1);
-                    mini = max({mini, m1, m2});
-                }
-                for(int k=l+1;k<=r;k++) {
-                    if(a[k]<0) {
-                        if(l<r) m2 = prod/(m1*a[k]);
-                        mini = max({mini, m1, m2});
-                    }
-                    m1 = m1*a[k];
-                    
-                }
-                mini= max(mini, m1);
-                ans = max(ans, mini);
+                if(prod==0) prod = a[i];
+                else prod = prod*a[i];
+                ans = max(ans, prod);
             }
-            if(j<n && a[j]==0)ans = max(ans, 0);
-            i = j+1;
         }
         
         return ans;
