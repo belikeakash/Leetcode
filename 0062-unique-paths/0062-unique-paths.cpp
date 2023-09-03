c4e1673ce99f69bfa19f96f0693b642a45b81ee4
Akash <76730100+belikeakash@users.noreply.github.com>
2023-09-03 15:13:24 +0530

    Time: 2 ms (44.36%), Space: 6.1 MB (78.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    
-    // int func(int i, int j, int m, int n) {
-    //     if(i==m-1 && j==n-1) return 1;
-    //     else if(i>=m || j>=n) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     return dp[i][j] = func(i+1,j,m,n) + func(i,j+1,m,n);
-    // }
+    int dp[101][101];
+    int func(int i, int j, int n, int m) {
+        if(i>=n || j>=m) return 0;
+        if(i==n-1 && j==m-1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = func(i+1, j, n, m) + func(i, j+1, n, m);
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1, 1));
-        //return func(0,0,m,n);
-        for(int i=1;i<m;i++) {
-            for(int j=1;j<n;j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        memset(dp, -1, sizeof(dp));
+        return func(0,0,m, n);
     }
 };
