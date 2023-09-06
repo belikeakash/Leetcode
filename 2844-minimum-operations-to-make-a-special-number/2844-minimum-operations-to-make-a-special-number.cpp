e2ed7bfb8b7887b6075ecff2fd777499d491fee7
Akash <76730100+belikeakash@users.noreply.github.com>
2023-09-06 14:12:15 +0530

    Time: 6 ms (23.96%), Space: 6.4 MB (28.94%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -9,11 +9,11 @@ public:
                 int x = (s[i]-'0')*10 + (s[j]-'0');
                 if(x==0 || x%25==0) {
                     ans = min(ans,(j-i-1)+(n-j-1));
-                    cout<<n<<" "<<i<<" "<<j<<" "<<ans<<endl;
+                   // cout<<n<<" "<<i<<" "<<j<<" "<<ans<<endl;
                 }
             }
         }
-        cout<<endl;
+        //cout<<endl;
         for(int i=0;i<n;i++) {
             if(s[i]=='0') ans = min(ans, n-1);
         }
